79714b2508640f8893c70f3b2db9d7667f752146
Jörg Oertel <tyskerjoerg@gmail.com>
2022-03-30 08:57:34 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import "./styles/App.css";
 
 function App() {
+  const URL = "https://jsonplaceholder.typicode.com/users";
   return (
     <div className="App">
       <h1>Monster Rolodex</h1>
