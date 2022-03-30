c57bc8dcbb97f9ce939de48366c7faffcc3c42b1
Jörg Oertel <tyskerjoerg@gmail.com>
2022-03-30 09:06:08 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import "./styles/App.css";
 
 function App() {
   const URL = "https://jsonplaceholder.typicode.com/users";
+  // <img
+  //   alt="monster"
+  //   src={`https://robohash.org/${id}?set=set2&size=180x180`}
+  // />;
   return (
     <div className="App">
       <h1>Monster Rolodex</h1>
