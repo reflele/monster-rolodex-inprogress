0aaa0dd25140154944418a4f98653808efae82a1
Jörg Oertel <tyskerjoerg@gmail.com>
2022-03-30 08:50:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Monster Rolodex</h1>
+    </div>
+  );
+}
+
+export default App;
